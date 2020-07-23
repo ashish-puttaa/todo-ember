0a1a76851b93ca5585d57741fc54f888b09d9953
Ashish Puttaa <ashishputtaa.98@gmail.com>
2020-07-23 14:19:45 +0530

    setup routes and controllers

--- app/router.js
+++ app/router.js
@@ -1,11 +1,15 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
-Router.map(function() {
+Router.map(function () {
+  this.resource("todos", function () {
+    this.route("edit");
+    this.route("new", { path: "/new/:todo_id" });
+  });
 });
 
 export default Router;
